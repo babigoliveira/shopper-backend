d57d6ad93fbe331dba5d26ecb92706b182e983c3
Bárbara Oliveira <barbaragdeoliveira@gmail.com>
2024-08-30 11:56:36 -0300

    test: fails duplicated measure

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,27 +4,34 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import * as mongoose from 'mongoose';
 import { MONGODB_CONNECTION_URI } from '../src/env';
+import { MeasureService } from '../src/measure/measure.service';
+import { ErrorResponseDto } from '../src/domain/dtos/error-response.dto';
+
+const encodedBase64SquareImage =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABQAAAALQAQMAAAD1s0' +
+  '8VAAAAA1BMVEX/AAAZ4gk3AAAAh0lEQVR42u3BMQEAAADCoPVPbQlPoAAAAAAAAA' +
+  'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
+  'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
+  'AAAAAAAAAAAAB4GsTfAAGc95RKAAAAAElFTkSuQmCC';
+
+const bodySample = {
+  image: encodedBase64SquareImage,
+  customer_code: 'customer_code',
+  measure_datetime: new Date().toISOString(),
+  measure_type: 'GAS',
+};
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  const encodedBase64SquareImage =
-    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABQAAAALQAQMAAAD1s0' +
-    '8VAAAAA1BMVEX/AAAZ4gk3AAAAh0lEQVR42u3BMQEAAADCoPVPbQlPoAAAAAAAAA' +
-    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
-    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
-    'AAAAAAAAAAAAB4GsTfAAGc95RKAAAAAElFTkSuQmCC';
-
-  const bodySample = {
-    image: encodedBase64SquareImage,
-    customer_code: 'customer_code',
-    measure_datetime: new Date().toISOString(),
-    measure_type: 'GAS',
-  };
-
   beforeAll(async () => {
     await mongoose.connect(MONGODB_CONNECTION_URI);
     await mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+    await app.close();
   });
 
   beforeEach(async () => {
@@ -40,46 +47,6 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
-
-  it('uploads image successfully', async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    })
-      .overrideProvider('GoogleAIFileManager')
-      .useValue({
-        uploadFile() {
-          return Promise.resolve({
-            file: { mimeType: '', uri: 'https://example.com' },
-          });
-        },
-      })
-      .overrideProvider('GenerativeModel')
-      .useValue({
-        generateContent() {
-          return {
-            response: {
-              text: () => '1234',
-            },
-          };
-        },
-      })
-      .compile();
-
-    const app = moduleFixture.createNestApplication();
-    await app.init();
-
-    return request(app.getHttpServer())
-      .post('/upload')
-      .send(bodySample)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body).toStrictEqual({
-          image_url: 'https://example.com',
-          measure_uuid: expect.any(String),
-          measure_value: 1234,
-        });
-      });
   });
 
   it('fails to parse image base64', () => {
@@ -113,6 +80,75 @@ describe('AppController (e2e)', () => {
       .expect({
         error_code: 'INVALID_DATA',
         error_description: 'Invalid date',
+      });
+  });
+
+  it('fails duplicated measure', async () => {
+    await app.close();
+
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(MeasureService)
+      .useValue({
+        findMeasure() {
+          return 'non null';
+        },
+      })
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    return request(app.getHttpServer())
+      .post('/upload')
+      .send(bodySample)
+      .expect(409)
+      .expect({
+        error_code: 'DOUBLE_REPORT',
+        error_description: 'Leitura do mês já realizada',
+      } as ErrorResponseDto);
+  });
+
+  it('uploads image successfully', async () => {
+    await app.close();
+
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider('GoogleAIFileManager')
+      .useValue({
+        uploadFile() {
+          return Promise.resolve({
+            file: { mimeType: '', uri: 'https://example.com' },
+          });
+        },
+      })
+      .overrideProvider('GenerativeModel')
+      .useValue({
+        generateContent() {
+          return {
+            response: {
+              text: () => '1234',
+            },
+          };
+        },
+      })
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    return request(app.getHttpServer())
+      .post('/upload')
+      .send(bodySample)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toStrictEqual({
+          image_url: 'https://example.com',
+          measure_uuid: expect.any(String),
+          measure_value: 1234,
+        });
       });
   });
 });
