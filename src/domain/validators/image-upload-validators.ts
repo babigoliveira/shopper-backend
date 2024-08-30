ac9c1a77063cc9425c6bf20363dcad36f16ad093
Bárbara Oliveira <barbaragdeoliveira@gmail.com>
2024-08-30 18:30:57 -0300

    refactor: fix lint errors

--- src/domain/validators/image-upload-validators.ts
+++ src/domain/validators/image-upload-validators.ts
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const isBase64 = require('is-base64');
+
 import { z } from 'zod';
 import { MEASURE_TYPES } from '../dtos/image-upload.dto';
 
